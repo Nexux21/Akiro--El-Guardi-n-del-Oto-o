83d3a52b06f71c72cc518ac9ab3ecb99f7d0d21b
Nexux21 <jesusserranocosio300@gmail.com>
2026-05-25 12:10:53 -0500

    1

--- Assets/scripts/enemy.cs
+++ Assets/scripts/enemy.cs
@@ -1,42 +1,36 @@
 using UnityEngine;
 
-public class enemy : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
-     public GameObject target;
-    public float speed;
+    public GameObject Target;
+    public float Speed;
     public float radiusAttack;
     public float radiusMovement;
-
     void Start()
     {
-        
+
     }
-
-
     void Update()
     {
         FollowTarget();
     }
-
     public void FollowTarget()
     {
-        Vector3 dir = (target.transform.position - transform.position).normalized;
-
-        if (Vector3.Distance(target.transform.position, transform.position) < radiusMovement)
+        Vector3 dir = (Target.transform.position - transform.position).normalized;
+        if (Vector3.Distance(Target.transform.position, transform.position) < radiusMovement)
         {
-            if (Vector3.Distance(target.transform.position, transform.position) < radiusAttack)
+            if (Vector3.Distance(Target.transform.position, transform.position) < radiusAttack)
             {
                 Debug.Log("atacando >:l ");
-
             }
             else
             {
-                transform.position += dir * speed * Time.deltaTime;
-
-
+                transform.position += dir * Speed * Time.deltaTime;
             }
         }
+
+
+
+
     }
 }
-
-

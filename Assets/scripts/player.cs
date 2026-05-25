bed91750c94f8a272ae8315db2c3af4a0b6b90d1
Nexux21 <jesusserranocosio300@gmail.com>
2026-05-25 10:55:10 -0500

    1

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 public class ejercicio : MonoBehaviour
 {
+    public GameObject target;
     public float HorizontalMovement;
     public float VerticalMovement;
     public float Speed;
+    public float smoothTime = 0.15f;
+    public Vector3 offset = new Vector3(0,0,-10);
+
+    private Vector3 velocity = Vector3.zero;
 
     void Start()
     {
@@ -17,9 +22,22 @@ public class ejercicio : MonoBehaviour
 
 
         MovementPlayer();
+        FollowTarget();
 
 
     }
+
+    public void FollowTarget()
+    {
+        if (target != null);
+        
+        Vector3 targetPosition = target.transform.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+
+
+    }
+
     public void MovementPlayer()
 
     {

93e975804f2e35a3b28ef0b438183d3b2e145dc1
Nexux21 <jesusserranocosio300@gmail.com>
2026-05-28 18:27:27 -0500

    1

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -2,15 +2,44 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    // Mi variable para controlar la velocidad desde el Inspector
+    public float speed = 10f;
 
-    // Update is called once per frame
+    private Vector2 inputsVector;
+    private Vector2 movementVector;
+
     void Update()
     {
-        
+        // Dividí mi lógica en funciones simples para cumplir con el Hito 2
+        LeerTeclado();
+        CalcularDireccion();
+        MoverPersonaje();
+    }
+
+    // Mi función para capturar las entradas del teclado usando Input.GetAxis
+    void LeerTeclado()
+    {
+        inputsVector.x = Input.GetAxisRaw("Horizontal");
+        inputsVector.y = Input.GetAxisRaw("Vertical");
+    }
+
+    // Aquí proceso mi vector de movimiento y lo normalizo para las diagonales
+    void CalcularDireccion()
+    {
+        if (inputsVector.magnitude > 1)
+        {
+            movementVector = inputsVector.normalized;
+        }
+        else
+        {
+            movementVector = inputsVector;
+        }
+    }
+
+    // Mi función final que cambia la posición sumando el vector de desplazamiento
+    void MoverPersonaje()
+    {
+        Vector3 desplazamiento = new Vector3(movementVector.x, movementVector.y, 0f);
+        transform.position = transform.position + (desplazamiento * speed * Time.deltaTime);
     }
 }

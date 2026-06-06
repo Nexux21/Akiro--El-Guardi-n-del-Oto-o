a8c01b8f68bc7fdf983b8116c45a589745296c8c
Nexux21 <jesusserranocosio300@gmail.com>
2026-06-05 22:23:24 -0500

    Update camera.cs

--- Assets/scripts/camera.cs
+++ Assets/scripts/camera.cs
@@ -2,14 +2,19 @@ using UnityEngine;
 
 public class camera : MonoBehaviour
 {
-    void Start()
-    {
-
-    }
+    public Transform Player;
 
     void Update()
     {
-
+        FollowPlayer();
     }
 
+    public void FollowPlayer()
+    {
+        transform.position = new Vector3(
+            Player.position.x,
+            Player.position.y,
+            -5
+        );
+    }
 }

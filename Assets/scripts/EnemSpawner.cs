cc153d0add91ac02b7439dcb3694311866c36c48
Nexux21 <jesusserranocosio300@gmail.com>
2026-06-08 12:18:21 -0500

    1

--- Assets/scripts/EnemSpawner.cs
+++ Assets/scripts/EnemSpawner.cs
@@ -1,33 +1,35 @@
 using UnityEngine;
 
-public class EnemSpawner : MonoBehaviour 
+public class EnemySpawner : MonoBehaviour
 {
     public GameObject EnemyPrefab;
-    public float spawnTimer = 3f;
+    public float spawnTimer = 0.5f;
     public float range;
 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
+    }
     void Update()
     {
         TimerMechanic();
     }
-
     public void TimerMechanic()
     {
-        spawnTimer = 3;
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer < 0)
+        {
+            SpawnEnemy();
+            spawnTimer = 0.5f;
+        }
     }
-
-
-
-public void SpawnEnemy()
-{
-
+    public void SpawnEnemy()
+    {
+        Vector3 randomDir = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0).normalized;
+        Vector3 fullLenghtDir = randomDir * Random.Range(0f, range);
+        GameObject enemy = Instantiate(EnemyPrefab, transform.position, Quaternion.identity);
+        enemy.transform.position += fullLenghtDir;
+    }
 }
-}
+        

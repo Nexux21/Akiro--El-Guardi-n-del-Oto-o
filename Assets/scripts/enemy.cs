d6b5c8dcdb7b01318b7b0e3e3d49052df001e791
Nexux21 <jesusserranocosio300@gmail.com>
2026-06-08 03:16:48 -0500

    1

--- Assets/scripts/enemy.cs
+++ Assets/scripts/enemy.cs
@@ -6,7 +6,10 @@ public class enemy : MonoBehaviour
     public float Speed;
     public float radiusAttack;
     public float radiusMovement;
-
+    public float damage;
+    public bool isAbleToAttack = true;
+    public float maxTime = 2f;
+    public float currentTime;
     void Start()
     {
 
@@ -15,10 +18,41 @@ public class enemy : MonoBehaviour
     void Update()
     {
         FollowTarget();
+        if (isAbleToAttack)
+        {
+            TimerToDoSmt();
+        }
     }
 
     public void FollowTarget()
     {
+        Vector3 targetPos = Target.transform.position;
+        Vector3 myPos = transform.position;
+
+        Vector3 direction = (targetPos - myPos).normalized;
+        if (Vector3.Distance(targetPos, myPos) > radiusMovement)
+        {
+            if (Vector3.Distance(targetPos, myPos) < radiusAttack)
+            {
+                if (isAbleToAttack)
+                    Debug.Log("dañandote");
+                Target.GetComponent<Player>().health -= damage;
+                isAbleToAttack = false;
+            }
+            else
+            {
+
+                transform.position += direction * Speed * Time.deltaTime;
+            }
+        }
+    }
+    public void TimerToDoSmt()
+    {        currentTime += Time.deltaTime;
+        if (currentTime >= maxTime)
+        {
+                     isAbleToAttack = true;
+            currentTime = 0;
+        }
         Vector3 dir = (Target.transform.position - transform.position).normalized;
         if (Vector3.Distance(Target.transform.position, dir) < radiusAttack)
         {
--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -4,6 +4,10 @@ public class Player : MonoBehaviour
 {
     public float Speed = 15f;
     public float health;
+    public float maxTime = 10;
+    public float currentTime;
+    public bool isAbilityAblive = true;
+    public GameObject BulletPrefab;
 
     void Start()
     {
@@ -13,7 +17,15 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+
+        if (!isAbilityAblive)
+        {
+            TimerToDoSmt();
+        }
         MovementPlayer();
+        Shoot();
+
+
     }
     public void MovementPlayer()
     {
@@ -28,5 +40,38 @@ public class Player : MonoBehaviour
         if (dir != Vector3.zero)
             transform.position += dir * Speed * Time.deltaTime;
     }
+
+
+    public void Shoot()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = (mousePos - transform.position);
+        direction.z = 0;
+        direction.Normalize();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(BulletPrefab, transform.position, Quaternion.identity);
+            bullet.transform.up = direction;
+        }
+    }
+    public void SimpleAttack()
+    {
+        if (isAbilityAblive)
+        {
+            isAbilityAblive = false;
+        }
+    }
+    public void TimerToDoSmt()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime >= maxTime)
+        {
+            isAbilityAblive = true;
+            currentTime = 0;
+        }
+    }
 }
+
+
 

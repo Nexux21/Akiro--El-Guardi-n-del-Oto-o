b59d97ebffbd49e06b54b2da2717093a84ead35b
Nexux21 <jesusserranocosio300@gmail.com>
2026-07-08 09:21:18 -0500

    1

--- Assets/scripts/Game_Manager_Vida.cs
+++ Assets/scripts/Game_Manager_Vida.cs
@@ -24,3 +24,32 @@ public class Game_Manager_Vida : MonoBehaviour
         
     }
 }
+
+
+
+
+
+
+
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarraVida : MonoBehaviour
+{
+    public Image rellenoBarraVida;
+    private PlayerController playerController;
+    private float vidaMaxima;
+    void Start()
+    {
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+        vidaMaxima = playerController.vida;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rellenoBarraVida.fillAmount = playerController.vida / vidaMaxima;
+    }
+}
